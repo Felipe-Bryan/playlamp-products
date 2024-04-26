6d7c494e5bc6ff8bca944746ab82db5108eae48e
Felipe Bryan <felipe.bryan@live.com>
2024-04-26 16:25:10 -0300

    new function: componentVisibility

--- src/functions/pages/startHome.ts
+++ src/functions/pages/startHome.ts
@@ -5,6 +5,7 @@ import { getStorageData } from '../general/getStorageData';
 import { startNewOrder } from '../page-functions/startNewOrder';
 import { placeCategoriesHome } from '../page-functions/placeCategoriesHome';
 import { placeProductsHome } from '../page-functions/placeProductsHome';
+import { componentVisibility } from '../page-functions/componentVisibility';
 
 export function startHome() {
   const home = document.getElementById('fullList')!;
@@ -22,7 +23,9 @@ export function startHome() {
   //       newOrder = startNewOrder(productsDB);
   //     }
   //   }
-
+  componentVisibility('newOrder', 'show');
+  componentVisibility('showOrder', 'show');
+  componentVisibility('orderTable', 'hide');
   placeCategoriesHome(categoriesDB, home);
   placeProductsHome(newOrder);
 }
